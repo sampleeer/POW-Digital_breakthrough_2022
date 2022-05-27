e92fdf6d2b66695e318c50f56aaf6669c02487d6
Globusin <karpov.globik@yandex.ru>
2022-05-27 23:50:44 +0300

    23:50

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -1,72 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:math';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List<Color> myColors = [
+    Colors.yellowAccent,
+    Colors.purple,
+  ];
+  @override
   Widget build(BuildContext context) {
+    const leftPanel = const Color(0xff28226b);
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(color: Colors.indigo[900]),
+        decoration: BoxDecoration(color: leftPanel),
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 30, 35, 10),
-              child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.menu_outlined,
-                    color: Colors.white,
-                    size: 50,
+            SizedBox(
+                height: 100,
+                child: InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.menu_outlined,
+                      size: 70,
+                      color: Colors.white,
+                    ))),
+            Container(
+              child: SizedBox(
+                  height: 100,
+                  child: InkWell(
+                    onTap: () async {
+                      final result = await FilePicker.platform.pickFiles();
+                    },
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.home,
+                          size: 60,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          'Главная',
+                          style: TextStyle(fontSize: 14, color: Colors.white),
+                        )
+                      ],
+                    ),
                   )),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 30, 35, 15),
-              child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.home_outlined,
-                    color: Colors.white,
-                    size: 50,
-                  )),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: Text(
-                'Главная',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 30, 35, 15),
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.stacked_bar_chart,
-                  color: Colors.white,
-                  size: 50,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-              child: Text('Статистика', style: TextStyle(color: Colors.white)),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 30, 35, 15),//
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.help_outline,
-                  color: Colors.white,
-                  size: 50,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 10),//
-              child: Text('Помощь', style: TextStyle(color: Colors.white)),
-            ),
+            SizedBox(
+                height: 100,
+                child: InkWell(
+                  focusColor: Colors.indigo.withOpacity(0.3),
+                  onTap: () {
+                    setState(() {});
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.stacked_bar_chart,
+                        size: 60,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Статистика',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      )
+                    ],
+                  ),
+                )),
+            SizedBox(
+                height: 100,
+                child: InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.help_outline,
+                        size: 60,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Помощь',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      )
+                    ],
+                  ),
+                )),
+            SizedBox(
+                height: 100,
+                width: 100,
+                child: InkWell(
+                  onTap: () {},
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.info_outline,
+                        size: 60,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Информация',
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      )
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
