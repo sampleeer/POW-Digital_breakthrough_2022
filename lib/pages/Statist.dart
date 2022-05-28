be9d20b2b281d4a826dbabc8fba723860327005d
Globusin <karpov.globik@yandex.ru>
2022-05-28 17:03:11 +0300

    17:03

--- lib/pages/Statist.dart
+++ lib/pages/Statist.dart
@@ -10,53 +10,28 @@ class Statist extends StatefulWidget {
 }
 
 class _StatistState extends State<Statist> {
-  List<DateTime?> _yourVariable = [DateTime.now()];
+  void _showDate() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2000),
+            lastDate: DateTime(2030))
+        .then((value) {
+      setState(() {
+        dateTime = value!;
+        ListTime.add(dateTime);
+      });
+    });
+  }
+
+  int count = 0;
+
+  List<DateTime> ListTime = [];
+  DateTime dateTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicWidth(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(120, 150, 0, 0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: CalendarDatePicker2WithActionButtons(
-                config: CalendarDatePicker2WithActionButtonsConfig(
-                  calendarType: CalendarDatePicker2Type.range,
-                  selectedDayHighlightColor: Colors.purple[800],
-                ),
-                initialValue: [],
-                onValueChanged: (values) => setState(
-                  () => _yourVariable = values,
-                ),
-              ),
-            ),
-            Expanded(
-                child: Column(
-              children: [
-                Container(
-                  color: Colors.cyan,
-                  child: SizedBox(
-                    width: 400,
-                    height: 100,
-                    child: Text('aboba'),
-                  ),
-                ),
-                Container(
-                  color: Colors.cyan,
-                  child: SizedBox(
-                    width: 400,
-                    height: 300,
-                    child: Text('aboba'),
-                  ),
-                )
-              ],
-            ))
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(home: Scaffold(body: Container()),);
   }
 }
 
